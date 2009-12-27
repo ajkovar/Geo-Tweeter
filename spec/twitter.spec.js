10ae2ae3924c6de8eb8a33c25582c1d8d3b6c49b
Alex <alex@alex-laptop.(none)>
2009-12-26 20:24:25 -0600

    rearranged logic of twitter module

--- spec/twitter.spec.js
+++ spec/twitter.spec.js
@@ -7,41 +7,65 @@ describe 'z.twitter'
 
 		mock_request().and_return(fixture('twitter.json'));
 		
-		searcher = new z.twitter.TweetSearcher();
+		aggregator = new z.twitter.TweetAggregator();
 	end
 
-	describe 'TweetSearcher'
+  	describe 'search()'
+  		it 'should make calls to twitter service for tweets'
+			$.should.receive('getJSON', 'once')
+			z.twitter.search("pie", 3, function(){});
+		end
+		
+		it 'should make multiple calls to twitter service if it extends beyond the limit per request'
+			$.should.receive('getJSON', 4)
+			z.twitter.search("pie", 310, function(){});
+		end
+	
+		it 'should pass tweets into callback function'
+			z.twitter.search("pie", 3, function(tweets){
+				tweets.length.should.equal 3
+				tweets[0].text.should.equal "geo"
+			});
+		end
+  	end
+  	
+  	describe 'mergeTweets()'
+  		it 'should remove duplicates'
+  			var tweets1 = JSON.parse(fixture('twitter.json')).results;
+  			var tweets2 = JSON.parse(fixture('twitter.json')).results;
+  			tweets1.length.should.equal 3
+  			tweets2.length.should.equal 3
+  			z.twitter.mergeTweets(tweets1, tweets2).length.should.equal 3
+  		end
+  		
+  		it 'should handle identical objects'
+  			var tweets = JSON.parse(fixture('twitter.json')).results;
+  			tweets.length.should.equal 3
+  			z.twitter.mergeTweets(tweets, tweets).length.should.equal 3
+  		end
+  	end
+  	
+  
+	describe 'filterNonGeocodes()'
+		it "should filter out tweets that don't have geocodes"
+			aggregator.search("pie", 3, function(tweets){
+				z.twitter.filterNonGeocodes(tweets).length.should.equal 2
+			});
+		end
+	end
+  	
+	describe 'TweetAggregator'
 
 		describe 'search()'
 	  
 			it 'should make calls to twitter service for tweets'
 				$.should.receive('getJSON', 'once')
-				searcher.search("pie", 3, function(){});
-			end
-		
-			it 'should make multiple calls to twitter service if it extends beyond the limit per request'
-				$.should.receive('getJSON', 4)
-				searcher.search("pie", 310, function(){});
-			end
-		
-			it 'should pass tweets into callback function'
-				searcher.search("pie", 3, function(tweets){
-					tweets.length.should.equal 3
-					tweets[0].text.should.equal "geo"
-				});
+				aggregator.search("pie", 3, function(){});
 			end
 		
 		end
 		
   	end
-  
-	describe 'filterNonGeocodes()'
-		it "should filter out tweets that don't have geocodes"
-			searcher.search("pie", 3, function(tweets){
-				z.twitter.filterNonGeocodes(tweets).length.should.equal 2
-			});
-		end
-	end
-  
+  	
 end
 
